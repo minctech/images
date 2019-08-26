db307a7ea6d9316cea9c2f6ebbbfe827a3846f4f
codegirl77 <rachelklinkatsis@gmail.com>
2019-08-26 14:26:50 -0700

    Fixed bugs in tests. Organized tests by components. Wrote three more tests based on functionality.

--- _tests_/test.js
+++ _tests_/test.js
@@ -10,15 +10,7 @@ Enzyme.configure({ adapter: new Adapter() });
 
 
 
-describe('Examining the syntax of Jest tests', () => {
-
-  it('sums numbers', () => {
-      expect(1 + 2).toEqual(3);
-      expect(2 + 2).toEqual(4);
-   });
-});
-
-// test to see if the main page renders and has heroImages
+// App functionality and rendering
 describe('App render', () => {
   let wrapper = mount(<App/>);
   it ('Should render app', () => {
@@ -26,28 +18,31 @@ describe('App render', () => {
     expect(wrapper.exists()).toBe(true);
   })
 
-  it ('displays five images on home page', () => {
-   expect(wrapper.contains('HeroImages')).toBe(true)
+  it ('displays heroImages on home page', () => {
+   expect(wrapper.find(HeroImages).length).toBe(1)
   })
-})
 
-//onComponentDidMount renders a listing
-describe('onComponentDidMount functionality', () => {
-  let wrapper;
-  // const props = {
+    // const props = {
   //   images: images[1]
   // };
-   wrapper = shallow(<App />);
-    it('should check `componentDidMount()`', () => {
-      const instance = wrapper.instance();
-      jest.spyOn(instance, 'getListing');
-      instance.componentDidMount();
-      expect(instance.getListing).toHaveBeenCalledTimes(1);
-    });
+  wrapper = shallow(<App />);
+  it('should check `componentDidMount()`', () => {
+    const instance = wrapper.instance();
+    jest.spyOn(instance, 'getListing');
+    instance.componentDidMount();
+    expect(instance.getListing).toHaveBeenCalledTimes(1);
+  });
+
+  it('when show images button is clicked onToggle should be triggered', () => {
+    const onToggle = jest.fn();
+    wrapper.find('.show-images').simulate('click');
+    expect(onToggle).toHaveReturned();
+   })
 })
 
-//test to see if clicking one of the heroImages triggers the onToggle function
-describe('Hero Images Click', () => {
+
+//testing for HeroImages functionality and rendering
+describe('HeroImages functionality', () => {
  const renderedComponent = shallow(<App/>);
   it('Hero Images should render', () => {
     const wrapper = mount(<HeroImages/>);
@@ -59,20 +54,29 @@ describe('Hero Images Click', () => {
    renderedComponent.find(HeroImages).simulate('click');
    expect(onToggle).toHaveReturned()
   })
+
+  it('when images are hovered on in heroImages css a css change happens', () => {
+    const cssChange = jest.fn();
+    const renderedComponent = shallow(<HeroImages />);
+    renderedComponent.find('.hero-image').simulate('mouseover');
+    expect(cssChange).toHaveReturned();
+   })
+
+   it('when an image is clicked the HeroImages, it will pop up the gallery on that specific image ', () => {
+    const onToggle = jest.fn();
+    const changeCurrentImage = jest.fn()
+    const renderedComponent = shallow(<HeroImages/>);
+    renderedComponent.find('.hero-image').simulate('click');
+    expect(onToggle).toHaveReturned();
+    expect(changeCurrentImage).toHaveReturned();
+   })
+
 })
 
-//test to see if clicking show images button triggers the on
-describe('test show images button triggers', () => {
- it('when show images button is clicked onToggle should be triggered', () => {
-  const onToggle = jest.fn();
-  const renderedComponent = shallow(<App />);
-  renderedComponent.find('.show-images').simulate('click');
-  expect(onToggle).toHaveReturned();
- })
-})
 
-//test for rendering of PopGallery
-describe('testing for proper rendering of PopGallery', () => {
+
+//PopGallery rendring and fucntionality
+describe('testing for proper rendering and functionality of PopGallery', () => {
   const renderedComponent = shallow(<PopGallery />);
   it('test to see if PopGallery has rendered well', () => {
     expect(renderedComponent.exists()).toBe(true)
@@ -89,11 +93,7 @@ describe('testing for proper rendering of PopGallery', () => {
   it('test to see if image tracker has rendered', () => {
     expect(renderedComponent.find('.image-tracker').length).toBeGreaterThanOrEqual(30)
   })
-})
 
-
-// test for next button click in PopGallery
-describe('next button click on PopGallery', () => {
   it('the next button is clicked it should move next in the gallery', () => {
     const nextButton = jest.fn();
     const renderedComponent = shallow(<PopGallery />);
@@ -101,22 +101,14 @@ describe('next button click on PopGallery', () => {
     expect(nextButton).toHaveReturned();
    })
 
-})
-
-// test for back button click in PopGallery
-describe('back button click on PopGallery', () => {
-  it('the back button is clicked it should move next in the gallery', () => {
+   it('the back button is clicked it should move next in the gallery', () => {
     const backButton = jest.fn();
     const renderedComponent = shallow(<PopGallery />);
     renderedComponent.find('.back').simulate('click');
     expect(backButton).toHaveReturned();
    })
 
-})
-
-//test for x button click in PopGallery
-describe('x button click on PopGallery', () => {
-  it('when x button is clicked onToggle should be triggered', () => {
+   it('when x button is clicked onToggle should be triggered', () => {
     const onToggle = jest.fn();
     const renderedComponent = shallow(<PopGallery />);
     renderedComponent.find('.x-button').simulate('click');
@@ -124,25 +116,34 @@ describe('x button click on PopGallery', () => {
    })
 })
 
-//test for onhover function on HeroImages
-describe('test on hover function on HeroImages', () => {
-  it('when images are hovered on in heroImages css a css change happens', () => {
-    const onToggle = jest.fn();
-    const renderedComponent = shallow(<PopGallery />);
-    renderedComponent.find('.hero-image').simulate('"mouseover"');
-    expect(onToggle).toHaveReturned();
-   })
 
-})
 
-//test for onhover for TinyGallery
+
+
+//TinyGallery functionailty and rendering
 describe('test on hover function on TinyGallery', () => {
+  const renderedComponent = shallow(<TinyGallery />);
+  it('test to see if TinyGallery has rendered well', () => {
+    expect(renderedComponent.exists()).toBe(true)
+  })
+
+  it('test to see if images have description div', () =>{
+    expect(renderedComponent.find(".tiny-image").length).toBeGreaterThanOrEqual(30)
+  })
+
   it('when images are hovered on in tiny gallery css a css change happens', () => {
-    const onToggle = jest.fn();
-    const renderedComponent = shallow(<TinyGallery />);
-    renderedComponent.find('.tiny-images').simulate('"mouseover"');
-    expect(onToggle).toHaveReturned();
+    const cssChange = jest.fn();
+    renderedComponent.find('.tiny-image').simulate('mouseover');
+    expect(cssChange).toHaveReturned();
+   })
+
+   it('when an image is clicked the TinyImages, it will jump to that specific image ', () => {
+    const changeCurrentImage = jest.fn()
+    const renderedComponent = shallow(<TinyGallery/>);
+    renderedComponent.find('.tiny-image').simulate('click');
+    expect(changeCurrentImage).toHaveReturned();
    })
 })
+
 
 
